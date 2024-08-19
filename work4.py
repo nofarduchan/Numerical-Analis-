d46dc8a5a7b02754bf655fd294f36c9a5d470e29
nofarduchan <nofardoc1@gmail.com>
2024-08-19 09:33:57 +0300

    fourth py work solution

--- work4.py
+++ work4.py
@@ -7,174 +7,79 @@ Nofar Duchan 322599424
 git_link: https://github.com/nofarduchan/Numerical-Analis-.git
 """
 
-# השתמשנו בפונקציות מקובץ עזר נוסף, כדי שהקובץ יתקמפל יש להוריד קובץ זה מהגיט
-from matrix_utility import *
 
-def linearInterpolation(table_points, point):
-    p = []
+def linear_interpolation(points, x):
+    """
+    Perform linear interpolation between the two closest points to x.
+
+    param points: List of points [(x0, y0), (x1, y1), ...]
+    param x: x value for which to find the interpolated y value
+    return: y value
+    """
+    # Sort points by x values
+    points = sorted(points)
+
+    # Find the two closest points
+    for i in range(len(points) - 1):
+        x0, y0 = points[i]
+        x1, y1 = points[i + 1]
+        if x0 <= x <= x1:
+            return y0 + (y1 - y0) * (x - x0) / (x1 - x0)
+
+    raise ValueError("x value is out of the range of provided points.")
+
+
+def polynomial_interpolation(points, x):
+    """
+    Perform polynomial interpolation using the provided points.
+
+    :param points: List of points [(x0, y0), (x1, y1), (x2, y2), ...]
+    :param x: x value for which to find the interpolated y value
+    :return: interpolated y value
+    """
+    n = len(points)
     result = 0
-    flag = 1
-    for i in range(len(table_points)):
-        p.append(table_points[i][0])
-    for i in range(len(p) - 1):
-        if i <= point <= i + 1:
-            x1 = table_points[i][0]
-            x2 = table_points[i + 1][0]
-            y1 = table_points[i][1]
-            y2 = table_points[i + 1][1]
-            result = (((y1 - y2) / (x1 - x2)) * point) + ((y2 * x1) - (y1 * x2)) / (x1 - x2)
-            print("\nThe approximation (interpolation) of the point ", point, " is: ", round(result, 4))
-            flag = 0
-    if flag:
-        x1 = table_points[0][0]
-        x2 = table_points[1][0]
-        y1 = table_points[0][1]
-        y2 = table_points[1][1]
-        m = (y1 - y2) / (x1 - x2)
-        result = y1 + m * (point - x1)
-        print( "\nThe approximation (extrapolation) of the point ", point, " is: ", round(result, 4))
-
-
-
-
-def lagrange_interpolation(x_data, y_data, x):
-    """
-    Lagrange Interpolation
-
-    Parameters:
-    x_data (list): List of x-values for data points.
-    y_data (list): List of y-values for data points.
-    x (float): The x-value where you want to evaluate the interpolated polynomial.
-
-    Returns:
-    float: The interpolated y-value at the given x.
-    """
-    n = len(x_data)
-    result = 0.0
 
     for i in range(n):
-        term = y_data[i]
+        xi, yi = points[i]
+        term = yi
         for j in range(n):
-            if i != j:
-                term *= (x - x_data[j]) / (x_data[i] - x_data[j])
+            if j != i:
+                xj, _ = points[j]
+                term *= (x - xj) / (xi - xj)
         result += term
 
     return result
 
-def GaussJordanElimination(matrix, vector):
+
+
+def lagrange_interpolation(points, x):
     """
-    Function for solving a linear equation using gauss's elimination method
-    :param matrix: Matrix nxn
-    :param vector: Vector n
-    :return: Solve Ax=b -> x=A(-1)b
+    param points: List of points [(x0, y0), (x1, y1), (x2, y2), ...]
+    param x: x value for which to find the interpolated y value
+    return: y value
     """
-    # Pivoting process
-    matrix, vector = RowXchange(matrix, vector)
-    # Inverse matrix calculation
-    invert = InverseMatrix(matrix, vector)
-    return MulMatrixVector(invert, vector)
+    total = 0
+    n = len(points)
+    # run on all the points
+    for i in range(n):
+        xi, yi = points[i]
+        term = yi
+        # run on all the points with point i
+        for j in range(n):
+            if j != i:
+                # split the points[j] in xj,yj.
+                xj, _ = points[j]
+                term *= (x - xj) / (xi - xj)
+        total += term
+    return total
 
 
 
 
-def UMatrix(matrix,vector):
-    """
-    :param matrix: Matrix nxn
-    :return:Disassembly into a  U matrix
-    """
-    # result matrix initialized as singularity matrix
-    U = MakeIMatrix(len(matrix), len(matrix))
-    # loop for each row
-    for i in range(len(matrix[0])):
-        # pivoting process
-        matrix, vector = RowXchageZero(matrix, vector)
-        for j in range(i + 1, len(matrix)):
-            elementary = MakeIMatrix(len(matrix[0]), len(matrix))
-            # Finding the M(ij) to reset the organs under the pivot
-            elementary[j][i] = -(matrix[j][i])/matrix[i][i]
-            matrix = MultiplyMatrix(elementary, matrix)
-    # U matrix is a doubling of elementary matrices that we used to reset organs under the pivot
-    U = MultiplyMatrix(U, matrix)
-    return U
-
-
-def LMatrix(matrix, vector):
-    """
-       :param matrix: Matrix nxn
-       :return:Disassembly into a  L matrix
-       """
-    # Initialize the result matrix
-    L = MakeIMatrix(len(matrix), len(matrix))
-    # loop for each row
-    for i in range(len(matrix[0])):
-        # pivoting process
-        matrix, vector = RowXchageZero(matrix, vector)
-        for j in range(i + 1, len(matrix)):
-            elementary = MakeIMatrix(len(matrix[0]), len(matrix))
-            # Finding the M(ij) to reset the organs under the pivot
-            elementary[j][i] = -(matrix[j][i])/matrix[i][i]
-            # L matrix is a doubling of inverse elementary matrices
-            L[j][i] = (matrix[j][i]) / matrix[i][i]
-            matrix = MultiplyMatrix(elementary, matrix)
-
-    return L
-
-
-def SolveLU(matrix, vector):
-    """
-    Function for deconstructing a linear equation by ungrouping LU
-    :param matrix: Matrix nxn
-    :param vector: Vector n
-    :return: Solve Ax=b -> x=U(-1)L(-1)b
-    """
-    matrixU = UMatrix(matrix)
-    matrixL = LMatrix(matrix)
-    return MultiplyMatrix(InverseMatrix(matrixU), MultiplyMatrix(InverseMatrix(matrixL), vector))
-
-
-def solveMatrix(matrixA,vectorb):
-    detA = Determinant(matrixA, 1)
-    # print( "\nDET(A) = ", detA)
-
-    if detA != 0:
-        # print("CondA = ", Cond(matrixA, InverseMatrix(matrixA, vectorb)))
-        # print("\nnon-Singular Matrix - Perform GaussJordanElimination")
-        result = GaussJordanElimination(matrixA, vectorb)
-        # print(np.array(result))
-        return result
-    else:
-        # print("Singular Matrix - Perform LU Decomposition\n")
-        # print("Matrix U: \n")
-        print(np.array(UMatrix(matrixA, vectorb)))
-        # print("\nMatrix L: \n")
-        print(np.array(LMatrix(matrixA, vectorb)))
-        # print("\nMatrix A=LU: \n")
-        result = MultiplyMatrix(LMatrix(matrixA, vectorb), UMatrix(matrixA, vectorb))
-        # print(np.array(result))
-        return result
-
-
-def polynomialInterpolation(table_points, x):
-    matrix = [[point[0] ** i for i in range(len(table_points))] for point in table_points] # Makes the initial matrix
-
-    b = [[point[1]] for point in table_points]
-
-    # print("The matrix obtained from the points: ", '\n', np.array(matrix))
-    # print("\nb vector: ", '\n',np.array(b))
-    matrixSol = solveMatrix(matrix, b)
-
-    result = sum([matrixSol[i][0] * (x ** i) for i in range(len(matrixSol))])
-    print("\nThe polynom:")
-    print('P(X) = '+'+'.join([ '('+str(matrixSol[i][0])+') * x^' + str(i) + ' ' for i in range(len(matrixSol))])  )
-    print(f"\nThe Result of P(X={x}) is:")
-    print(result)
-    return result
-
-
-if __name__ == '__main__':
-
+def main():
     # Defining the point pairs
-    points = [(0, 0), (1, 0.8415), (2, 0.9093)]
+    points =  [(0, 0), (1, 0.8415), (2, 0.9093)]
 
     # Getting the x value from the user
     x = 1.28
@@ -191,19 +96,23 @@ if __name__ == '__main__':
         # if len(points) < 2:
         #     print("At least 2 points are required for linear interpolation.")
         # else:
-        result = linearInterpolation(points, x)
+            result = linear_interpolation(points, x)
+            print(f"The estimated value of y for x = {x} using linear interpolation is: {result}")
     elif method == 1:
         # if len(points) < 3:
         #     print("At least 3 points are required for polynomial interpolation.")
         # else:
-        result = polynomialInterpolation(points, x)
+            result = polynomial_interpolation(points, x)
+            print(f"The estimated value of y for x = {x} using polynomial interpolation is: {result}")
     elif method == 2:
         # if len(points) < 3:
         #     print("At least 3 points are required for polynomial interpolation.")
         # else:
-        x_data = [point[0] for point in points]
-        y_data = [point[1] for point in points]
-        result = lagrange_interpolation(x_data, y_data, x)
-        print(f"The approximation (extrapolation) of x = {x} using Lagrange interpolation is: {result}")
+            result = lagrange_interpolation(points, x)
+            print(f"The estimated value of y for x = {x} using Lagrange interpolation is: {result}")
     else:
         print("Invalid method selection, please try again.")
+
+
+if __name__ == "__main__":
+    main()
